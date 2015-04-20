d01b0eed1b0c57f9fd1c55de2058c6ddace3b4dd
Dave Watkins <davidwatkins73@gmail.com>
2015-04-20 12:44:49 +0100

    Added timer intervals to Timeago Widget

--- react-lurn/app/basic/Timeago.jsx
+++ react-lurn/app/basic/Timeago.jsx
@@ -2,11 +2,27 @@ var React = require("react");
 var Moment = require("moment");
 
 var TimeAgo = React.createClass({
+
     propTypes: {
         date : React.PropTypes.oneOfType([
             React.PropTypes.object,
             React.PropTypes.number
-        ])
+        ]),
+        updateInterval : React.PropTypes.number
+    },
+
+    getDefaultProps() {
+        updateInterval: 1
+    },
+
+    componentWillMount() {
+        this.intervalRef = setInterval(() => {
+            this.forceUpdate();
+        }, this.props.updateInterval * 1000);
+    },
+
+    componentWillUnmount() {
+        clearInterval(this.interval.ref);
     },
 
     render() {
