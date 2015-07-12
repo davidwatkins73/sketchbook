31b358d531f3b4a9da38eb1788e646fffd8317b2
David Watkins <davidwatkins73@gmail.com>
2015-07-12 12:40:22 +0100

    Rewriting into es6 class style.

---
+++ material-react/app/SomeAwesomeComponent.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ThemeToggle from './ThemeToggle.jsx';
+import _ from 'lodash';
+import mui from 'material-ui';
+import MyComponent from './MyComponent.jsx';
+import AnotherComponent from './AnotherComponent.jsx';
+import { bus, Actions } from './EventSys';
+let { AppBar, RaisedButton, Checkbox } = mui;
+let ThemeManager = new mui.Styles.ThemeManager();
+
+
+class SomeAwesomeComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { showTimer : true };
+    }
+
+    componentWillMount() {
+        bus.filter(x => x.type === Actions.UPDATE_THEME)
+            .subscribe(x => {
+                ThemeManager.setComponentThemes(x.data);
+                this.forceUpdate()
+            });
+
+        bus.filter(x => x.type === Actions.TOGGLE_COUNTER)
+            .subscribe(x => {
+                let s = this.state;
+                let newState = _.extend(s, { showTimer : ! s.showTimer });
+                console.log("ns", newState)
+                this.setState(newState);
+            });
+    }
+
+    getChildContext() {
+        return {
+            muiTheme: ThemeManager.getCurrentTheme()
+        };
+    }
+
+    onClick() {
+        bus.onNext({type: Actions.TOGGLE_COUNTER, data: {}});
+    }
+
+    render() {
+        let maybeTimer = this.state.showTimer ? <MyComponent/> : null;
+        return (
+            <div>
+                <AppBar title='Title' iconClassNameLeft="muidocs-icon-action-home"/>
+                {maybeTimer}
+                <ThemeToggle/>
+                <RaisedButton label="Default" onClick={this.onClick} />
+                <AnotherComponent/>
+            </div>
+        );
+    }
+
+};
+
+SomeAwesomeComponent.childContextTypes = {muiTheme: React.PropTypes.object };
+
+export default SomeAwesomeComponent;
